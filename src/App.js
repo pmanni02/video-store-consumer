f50636bcbac39a04915adb1f5836beaf26202f6f
Lily Sky <lilyxsky@gmail.com>
2018-06-18 15:04:54 -0700

    local api request works; cannot render yet

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: [],
+    };
+  }
+
+  componentDidMount = () => {
+    let query =  'http://localhost:3000/movies'
+    console.log(query)
+
+    axios.get(query)
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          movies: response.data
+        })
+      })
+      .catch((error) => {
+        this.setState({
+          error: error.message
+        });
+        if (error.message) {
+          this.renderError();
+          console.log(error.message);
+        }
+      }
+    );
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,7 +42,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.state.movies}
         </p>
       </div>
     );
