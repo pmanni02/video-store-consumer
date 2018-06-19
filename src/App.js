302247e93beb7447a2e119ff247b308aad1dfe96
Lily Sky <lilyxsky@gmail.com>
2018-06-19 11:01:28 -0700

    prepped for merge

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <div className="App-intro">
-        <Rental/>
+        
         </div>
       </div>
     );
--- src/components/Rental.js
+++ src/components/Rental.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import RentalList from './RentalList';
+import NewRentalForm from './NewRentalForm';
 import CustomerList from './CustomerList';
 
 class Rental extends Component {
-  constructor() {
-    super();
-    this.state = {
-      selectedMovie: 'None',
-      selectedCustomer: 'None',
-    };
-  }
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     selectedMovie: 'None',
+  //     selectedCustomer: 'None',
+  //   };
+  // }
 
   render() {
     return (
       <div>
+        <div>
+          <NewRentalForm />
+        </div>
         <div>
           <CustomerList />
         </div>
