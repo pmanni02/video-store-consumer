71cd79ea62aee4ae5538d2c5fc1b573e9ab9d83b
Phoebe <phoebe.manning8@gmail.com>
2018-06-18 17:02:01 -0700

    adds axios get request to Tmbd

--- src/components/Tmbd.js
+++ src/components/Tmbd.js
@@ -12,8 +12,16 @@ class Tmbd extends Component {
   }
 
   getMovies = (search) => {
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=4bc8e1bdfa9b893a8f74030d299fd75d&query=';
     console.log('In TMBD.js');
     console.log(`Search input: ${search}`);
+    axios.get(`${url}${search}`)
+      .then( (response) => {
+        console.log(response.data.results);
+      })
+      .catch( (error) => {
+        console.log(error);
+      });
   }
 
   render(){
